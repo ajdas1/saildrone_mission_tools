a84278a70685826924b1197fd1517c5caf9015bd
ajdas1 <ajdas1@gmail.com>
2023-08-15 12:15:26 -0400

    black

--- scripts/get_latest_glider_position.py
+++ scripts/get_latest_glider_position.py
@@ -4,7 +4,7 @@ import urllib.request
 from conversions import coordinate_degrees_minutes_to_decimal
 from datetime import datetime, timedelta
 
-### URLs for glider positions were provided by Catherine Edwards. 
+### URLs for glider positions were provided by Catherine Edwards.
 
 
 print()
@@ -16,9 +16,17 @@ tmp = page.split("\\n")
 category = [tm.split(" ")[0][:-1].split("'")[-1] for tm in tmp if len(tm) > 5]
 
 
-lat = [coordinate_degrees_minutes_to_decimal(tm.split(" ")[1]) for tm in tmp if len(tm) > 5]
-lon = [coordinate_degrees_minutes_to_decimal(tm.split(" ")[2]) for tm in tmp if len(tm) > 5]
-time = [log_start_time + timedelta(seconds=float(tm.split(" ")[3])) for tm in tmp if len(tm) > 5]
+lat = [
+    coordinate_degrees_minutes_to_decimal(tm.split(" ")[1]) for tm in tmp if len(tm) > 5
+]
+lon = [
+    coordinate_degrees_minutes_to_decimal(tm.split(" ")[2]) for tm in tmp if len(tm) > 5
+]
+time = [
+    log_start_time + timedelta(seconds=float(tm.split(" ")[3]))
+    for tm in tmp
+    if len(tm) > 5
+]
 
 df = pd.DataFrame([], columns=["category", "lat", "lon", "time"])
 df.category = category
@@ -28,12 +36,12 @@ df.time = time
 df = df[df.category == "GPS"]
 
 print(
-    "Latest coordinates for Franklin (lat, lon) \n" + 
-    f"({df[df.time == max(df.time)].lat.values[0]:.4f}, " + 
-    f"{df[df.time == max(df.time)].lon.values[0]:.4f}) @ " + 
-    f"{df[df.time == max(df.time)].time.dt.strftime('%Y-%m-%d %H:%M UTC').values[0]}" + 
-    "\n"
-    )
+    "Latest coordinates for Franklin (lat, lon) \n"
+    + f"({df[df.time == max(df.time)].lat.values[0]:.4f}, "
+    + f"{df[df.time == max(df.time)].lon.values[0]:.4f}) @ "
+    + f"{df[df.time == max(df.time)].time.dt.strftime('%Y-%m-%d %H:%M UTC').values[0]}"
+    + "\n"
+)
 
 # Franklin
 url_glider = "http://dockserver.skio.uga.edu/images/franklin/franklin_wptlatlonDate.txt"
@@ -42,9 +50,17 @@ tmp = page.split("\\n")
 category = [tm.split(" ")[0][:-1].split("'")[-1] for tm in tmp if len(tm) > 5]
 
 
-lat = [coordinate_degrees_minutes_to_decimal(tm.split(" ")[1]) for tm in tmp if len(tm) > 5]
-lon = [coordinate_degrees_minutes_to_decimal(tm.split(" ")[2]) for tm in tmp if len(tm) > 5]
-time = [log_start_time + timedelta(seconds=float(tm.split(" ")[3])) for tm in tmp if len(tm) > 5]
+lat = [
+    coordinate_degrees_minutes_to_decimal(tm.split(" ")[1]) for tm in tmp if len(tm) > 5
+]
+lon = [
+    coordinate_degrees_minutes_to_decimal(tm.split(" ")[2]) for tm in tmp if len(tm) > 5
+]
+time = [
+    log_start_time + timedelta(seconds=float(tm.split(" ")[3]))
+    for tm in tmp
+    if len(tm) > 5
+]
 
 df = pd.DataFrame([], columns=["category", "lat", "lon", "time"])
 df.category = category
@@ -54,9 +70,9 @@ df.time = time
 df = df[df.category == "GPS"]
 
 print(
-    "Latest coordinates for UNIT_1091 (lat, lon) \n" + 
-    f"({df[df.time == max(df.time)].lat.values[0]:.4f}, " + 
-    f"{df[df.time == max(df.time)].lon.values[0]:.4f}) @ " + 
-    f"{df[df.time == max(df.time)].time.dt.strftime('%Y-%m-%d %H:%M UTC').values[0]}"
+    "Latest coordinates for UNIT_1091 (lat, lon) \n"
+    + f"({df[df.time == max(df.time)].lat.values[0]:.4f}, "
+    + f"{df[df.time == max(df.time)].lon.values[0]:.4f}) @ "
+    + f"{df[df.time == max(df.time)].time.dt.strftime('%Y-%m-%d %H:%M UTC').values[0]}"
     + "\n"
-    )
+)
--- util/conversions.py
+++ util/conversions.py
@@ -1,22 +1,21 @@
-
 def coordinate_degrees_minutes_to_decimal(string: str):
     """
-    Args: 
+    Args:
     - string: location in format DDMM.MMM
 
     Returns:
     - decimal representation of given location: DD.DDDD
     """
-    
+
     flt = float(string)
 
     if flt > 0:
-        deg = int(flt/100.)
+        deg = int(flt / 100.0)
         min = flt % 100
-        res = float(deg) + float(min)/60
+        res = float(deg) + float(min) / 60
     elif flt < 0:
-        deg = abs(int(flt/100))
+        deg = abs(int(flt / 100))
         min = abs(flt) % 100
-        res = - (float(deg) + float(min)/60)
+        res = -(float(deg) + float(min) / 60)
 
     return res
