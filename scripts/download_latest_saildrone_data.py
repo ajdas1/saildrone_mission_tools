c58d2ad70dc73f8d97aac3ffcfd4bb425f500644
ajdas1 <ajdas1@gmail.com>
2023-08-17 11:43:30 -0400

    black

--- scripts/download_latest_saildrone_data.py
+++ scripts/download_latest_saildrone_data.py
@@ -5,6 +5,7 @@ import urllib.error
 
 import importlib
 import paths
+
 importlib.reload(paths)
 from paths import check_for_dir_create, read_yaml_config, repo_path, saildrone_archive
 
@@ -28,7 +29,9 @@ for sd in update_saildrones:
     print(f"Downloading the latest file for SD-{sd}.")
     filename = f"sd{sd}_hurricane_2023.ncCF"
     try:
-        urllib.request.urlretrieve(f"{saildrone_archive}{filename}", f"{saildrone_dir}{os.sep}{filename[:-2]}")
+        urllib.request.urlretrieve(
+            f"{saildrone_archive}{filename}", f"{saildrone_dir}{os.sep}{filename[:-2]}"
+        )
     except urllib.error.HTTPError:
         print("Could not download the latest data.")
         continue
