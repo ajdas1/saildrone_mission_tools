472e2d5942a0792aa9619808c4eb20dc2a336ed6
ajdas1 <ajdas1@gmail.com>
2023-08-21 14:49:45 -0400

    process all current atcf data

--- scripts/download_atcf_current_systems_data.py
+++ scripts/download_atcf_current_systems_data.py
@@ -11,7 +11,6 @@ importlib.reload(paths)
 import read_url
 importlib.reload(read_url)
 
-from datetime import datetime
 from paths import atcf_btk_archive, atcf_storm_archive, check_for_dir_create, read_yaml_config, repo_path
 from read_url import get_files_at_url, retrieve_url_file
 
@@ -46,20 +45,20 @@ invest_records = [rec for rec in all_records if re.match(f".*aal9.", rec)]
 storm_records = [rec for rec in all_records if re.match(f".*aal[^9]\d", rec)]
 
 # download all of them, will weed out in the next step
+print(f"Downloading ATCF data for invests.")
 for invest in invest_records:
-    print(f"Downloading ATCF data for invests.")
     filename = invest.split("/")[-1]
     retrieve_url_file(invest, f"{invest_dir}{os.sep}{filename}")
 
+print(f"Downloading ATCF data for storms.")
 for storm in storm_records:
-    print(f"Downloading ATCF data for storms.")
     filename = storm.split("/")[-1]
-    retrieve_url_file(invest, f"{storm_dir}{os.sep}{filename}")
+    retrieve_url_file(storm, f"{storm_dir}{os.sep}{filename}")
 
 # for best track
 all_records = get_files_at_url(url=atcf_btk_archive)
 btk_records = [rec for rec in all_records if ".dat" in rec and "bal" in rec]
+print(f"Downloading ATCF best track data.")
 for btk in btk_records:
-    print(f"Downloading ATCF best track data.")
     filename = btk.split("/")[-1]
-    retrieve_url_file(invest, f"{btk_dir}{os.sep}{filename}")
+    retrieve_url_file(btk, f"{btk_dir}{os.sep}{filename}")
