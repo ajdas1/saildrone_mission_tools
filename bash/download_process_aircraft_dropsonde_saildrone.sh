79223f7477027d7b2792c43d6d25049447580c94
ajdas1 <ajdas1@gmail.com>
2023-09-11 13:30:48 -0400

    dropsondes full example

---
+++ bash/download_process_aircraft_dropsonde_saildrone.sh
@@ -0,0 +1,7 @@
+
+
+python ../scripts/download_latest_saildrone_data.py
+
+python ../scripts/download_aircraft_recon_dropsonde_full.py
+
+python ../scripts/identify_dropsondes_in_range_of_saildrone.py
